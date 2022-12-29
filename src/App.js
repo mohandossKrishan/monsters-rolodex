14712902e0420e3b792deca238ba06f063c0ed5f
mohandoss28@gmail.com <mohandoss28@gmail.com>
2022-12-29 12:11:49 +0530

    Lesson35

--- src/App.js
+++ src/App.js
@@ -6,22 +6,34 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monster1 : {
-        name: 'Linda'},
-      monster2 : {
-        name: 'Frank'},
-      monster3 : {
-        name: 'Jack'},
-      };
+      monsters : [
+        {
+          name: 'Linda',
+          id: '1234',
+        },
+        {
+          name: 'Frank',
+          id: '12345',
+        },
+        { 
+          name: 'Jack',
+          id: '123456',
+        },
+      ],
+    };
   }
   render() {
   return (
     <div className="App">
-      <h1>{this.state.monster1.name}</h1>
-      <h1>{this.state.monster2.name}</h1>
-      <h1>{this.state.monster3.name}</h1>
+      {this.state.monsters.map((monster) => {
+          return (
+            <div key= {monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
     </div>
   );
-}
+  }
 }
 export default App;
